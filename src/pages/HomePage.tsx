7655bd7bfbf6cfff49cd6944410171b3895cf791
Charbelghaleb <charbel@churchillpaintingcorp.com>
2025-06-21 14:48:37 -0400

    Update homepage meta title and description

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -167,8 +167,8 @@ function HomePage() {
   return (
     <>
       <Helmet>
-        <title>Terra Nuova | NYC Epoxy & Polyurea Floor Coatings</title>
-        <meta name="description" content="Terra Nuova installs high-performance epoxy and polyurea flooring across NYC. Residential, commercial & industrial coatings. Free estimates." />
+        <title>Terra Nuova | Epoxy Floor Coatings in NYC</title>
+        <meta name="description" content="Professional epoxy floor coatings in NYC by Terra Nuova. Durable, stylish floors for garages, basements, shops & more. Free estimates available." />
         
         {/* LocalBusiness Schema Markup */}
         <script type="application/ld+json">
