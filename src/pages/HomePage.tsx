30e14e996f9b1bec204f67de11e51e7f45f80891
Charbelghaleb <charbel@churchillpaintingcorp.com>
2025-06-22 13:34:25 -0400

    Deploy application to Netlify

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -390,7 +390,7 @@ function HomePage() {
                 id="solution-title-1"
                 style={getScrollAnimationStyle('solution-title-1', 'slideUp', 3)}
               >
-                The art of
+                Transform Your Space with
               </h3>
               <h3 
                 className="text-2xl md:text-4xl lg:text-5xl font-black mb-4 md:mb-6 scroll-reveal"
